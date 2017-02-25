a4e586db2f82df49e102debd15ec20c90e9883e9
Xavier Mendez <me@jmendeth.com>
2017-02-25 16:21:21 +0100

    Finish circular queue tests

--- src/ast/practica1/CircularQueue.java
+++ src/ast/practica1/CircularQueue.java
@@ -12,7 +12,7 @@ public class CircularQueue<T> implements Queue<T> {
 
     public CircularQueue(int n) {
     }
-    
+
     @Override
     public int size() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -57,18 +57,30 @@ public class CircularQueue<T> implements Queue<T> {
     public Iterator<T> iterator() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     public static void main(String[] args) {
-        final CircularQueue<String> queue = new CircularQueue<>(4);
-        
+        final CircularQueue<String> queue = new CircularQueue<>(3);
+
         // Initial state
         expect(queue.size(), 0);
-        //TODO
-        
+        expect(queue.empty());
+        expect(!queue.full());
+        expect(queue.hasFree(3));
+        expect(!queue.hasFree(4));
+        expect(queue.peekFirst(), null);
+        expect(queue.peekLast(), null);
+
         // Normal get / put
         queue.put("test");
+        expect(queue.size(), 1);
+        expect(!queue.empty());
+        expect(!queue.full());
+        expect(queue.hasFree(2));
+        expect(!queue.hasFree(3));
+        expect(queue.peekFirst(), "test");
+        expect(queue.peekLast(), "test");
         expect(queue.get(), "test");
-        
+
         // Overflow queue
         queue.put("one");
         queue.put("two");
@@ -79,7 +91,14 @@ public class CircularQueue<T> implements Queue<T> {
         } catch (IllegalStateException ex) {
             expect(ex.getMessage(), "Queue is full");
         }
-        
+
+        expect(queue.size(), 3);
+        expect(queue.full());
+        expect(!queue.empty());
+        expect(queue.hasFree(0));
+        expect(!queue.hasFree(1));
+        expect(!queue.hasFree(3));
+
         // Underflow queue
         expect(queue.get(), "three");
         expect(queue.get(), "two");
@@ -90,12 +109,26 @@ public class CircularQueue<T> implements Queue<T> {
         } catch (IllegalStateException ex) {
             expect(ex.getMessage(), "Queue is full");
         }
-        
+
+        expect(queue.size(), 0);
+        expect(queue.empty());
+        expect(!queue.full());
+        expect(queue.hasFree(3));
+        expect(!queue.hasFree(4));
+        expect(queue.peekFirst(), null);
+        expect(queue.peekLast(), null);
+
         // Null elements
         queue.put(null);
         queue.put(null);
         expect(queue.get(), null);
         expect(queue.size(), 1);
+        expect(!queue.empty());
+        expect(!queue.full());
+        expect(queue.hasFree(2));
+        expect(!queue.hasFree(3));
+        expect(queue.peekFirst(), null);
+        expect(queue.peekLast(), null);
     }
-    
+
 }
