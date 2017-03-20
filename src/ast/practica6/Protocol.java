8b33c79aed647f371c3064a63437f08ac30360c1
Xavier Mendez <me@jmendeth.com>
2017-03-20 21:16:34 +0100

    Exercise 1

--- src/ast/practica6/Protocol.java
+++ src/ast/practica6/Protocol.java
@@ -1,10 +1,11 @@
 package ast.practica6;
 
-import ast.util.FDuplexChannel;
 import ast.protocols.tcp.TCPSegment;
 
 import ast.logging.Log;
 import ast.logging.LogFactory;
+
+import ast.util.FDuplexChannel;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -33,28 +34,33 @@ public class Protocol {
     public TSocket openWith(int localPort, int remotePort) {
         lk.lock();
         try {
-            // A completar per l'estudiant (veieu practica 3 ó 5):
-            ...
+            if (getMatchingTSocket(localPort, remotePort) != null)
+                return null;
+            TSocket socket = new TSocket(this, localPort, remotePort);
+            sockets.add(socket);
+            return socket;
         } finally {
             lk.unlock();
         }
     }
 
     protected void ipInput(TCPSegment segment) {
-        // A completar per l'estudiant (veieu practica 3 ó 5):
-        ...
+        TSocket socket = getMatchingTSocket(segment.getDestinationPort(), segment.getSourcePort());
+        if (socket != null) socket.processReceivedSegment(segment);
     }
 
     protected TSocket getMatchingTSocket(int localPort, int remotePort) {
         lk.lock();
         try {
-            // A completar per l'estudiant (veieu practica 3 ó 5):
-            ...
+            for (TSocket socket : sockets) {
+                if (socket.localPort == localPort && socket.remotePort == remotePort)
+                    return socket;
+            }
+            return null;
         } finally {
             lk.unlock();
         }
     }
-
 
     class ReceiverTask implements Runnable {
         @Override
