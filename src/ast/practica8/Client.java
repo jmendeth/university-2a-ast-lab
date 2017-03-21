5ade6ac836f183134748c34398b63768bd5c8fb5
Xavier Mendez <me@jmendeth.com>
2017-03-21 11:16:32 +0100

    Prepare for practica-8

---
+++ src/ast/practica8/Client.java
@@ -0,0 +1,15 @@
+package ast.practica8;
+
+/**
+ * Main class for the client.
+ * Arguments: {@code <server IP>}
+ *
+ * @author Xavier Mendez
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        //TODO
+    }
+
+}
---
+++ src/ast/practica8/Server.java
@@ -0,0 +1,14 @@
+package ast.practica8;
+
+/**
+ * Main class for the server.
+ *
+ * @author Xavier Mendez
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        //TODO
+    }
+
+}
